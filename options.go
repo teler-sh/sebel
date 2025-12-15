2c48fadc80f06433490592608cc5dbaf6a75b124
Dwi Siswanto <25837540+dwisiswant0@users.noreply.github.com>
2025-12-16 01:25:45 +0700

    feat: add writer output with custom formatter support (#1)
    * feat: add writer output with custom formatter support
    
    Add `Output` and `Formatter` fields to `Options`
    for logging blacklist detections. When `Output` is
    set, detection events are written using either a
    user-provided `Formatter` function or a default
    format.
    
    The `Formatter` callback receives the full
    `sslbl.Record` and fingerprint, allowing users to
    customize output format for their logging needs.
    
    Signed-off-by: Dwi Siswanto <git@dw1.io>
    
    * docs: checks TODO
    
    Signed-off-by: Dwi Siswanto <git@dw1.io>
    
    ---------
    
    Signed-off-by: Dwi Siswanto <git@dw1.io>

--- options.go
+++ options.go
@@ -1,12 +1,25 @@
 package sebel
 
+import (
+	"io"
+
+	"github.com/teler-sh/sebel/pkg/sslbl"
+)
+
 // Options holds configuration settings for the [Sebel] package.
 type Options struct {
 	// DisableSSLBlacklist, when set to true, disables SSL/TLS certificate
 	// blacklist checks.
 	DisableSSLBlacklist bool
 
+	// Output specifies the writer for logging blacklist detections.
+	// If nil, no output is written.
+	Output io.Writer
+
+	// Formatter customizes the output format for blacklist detections.
+	// If nil, a default format is used.
+	Formatter func(record *sslbl.Record, fingerprint string) string
+
 	// TODO(dwisiswant0): Add these fields
 	// DisableHostBlacklist bool
-	// Output               io.Writer
 }
